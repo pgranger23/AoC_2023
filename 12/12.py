5e0bda8b515429d99c307a509042ee37fc70bb42
Pierre GRANGER <pgranger23@gmail.com>
2023-12-18 23:26:48 +0100

    Clean day 12

--- 12/12.py
+++ 12/12.py
@@ -10,70 +10,21 @@ def parse_input(iname:str) -> np.ndarray:
     lines = [[line[0], tuple(map(int, line[1].split(',')))] for line in lines]
     return lines
 
-def count_damaged(group):
-    return group.count('#')
-
-def get_unk_pos(line):
-    pos_list = []
-    for i, ch in enumerate(line):
-        if ch == '?':
-            pos_list.append(i)
-    return pos_list
-
-def is_valid(line):
-    groups = re.findall(r'#+', line)
-    if len(groups) != 1:
-        return False
-    return True
-
-@cache
-def bruteforce_single(group, count):
-    nb_damaged = count_damaged(group)
-    unk_pos = get_unk_pos(group)
-
-    if nb_damaged > count:
-        return 0
-    if nb_damaged == count:
-        return is_valid(group)*1
-
-    nb_to_add = count - nb_damaged
-
-    nb_ok = 0
-    ref = np.array([*group])
-    for idxs in combinations(unk_pos, nb_to_add):
-        proposition = ref.copy()
-        proposition[list(idxs)] = '#'
-        nb_ok += is_valid(''.join(proposition.tolist()))
-    return nb_ok
-
 @cache
 def nb_possibilities(group, count_target):
     group = group.strip('.')
-    if len(count_target) == 1:
-        if '.' in group:
-            new_groups = group.split('.')
-            new_groups = [g for g in new_groups if g != '']
-            total = 0
-            for i in range(len(new_groups)):
-                count = np.zeros(len(new_groups), dtype=int)
-                count[i] = count_target[0]
-                total = total + np.product([nb_possibilities(new_groups[j], (count[j],)) for j in range(len(new_groups))])
-            return total
-        if '#' in group:
-            return bruteforce_single(group, count_target[0])
-        if len(group) < count_target[0]:
-            return 0
-        if count_target[0] == 0:
-            return 1
-        return len(group) + 1 - count_target[0] #Formula with only ? in group
+    if not count_target:
+        return 0 if "#" in group else 1
+    if not group:
+        return 1 if not count_target else 0
     
-    if sum(count_target) >= len(group):
+    if sum(count_target) > len(group):
         return 0
     if group[0] == '?':
         return nb_possibilities('#' + group[1:], count_target) + nb_possibilities(group[1:], count_target)
-    if group[count_target[0]] == '#':
+    if (len(group) != count_target[0] and group[count_target[0]] == '#') or '.' in group[:count_target[0]]:
         return 0
-    return nb_possibilities(group[:count_target[0]], count_target[:1])*nb_possibilities(group[count_target[0] + 1:], count_target[1:])
+    return nb_possibilities(group[count_target[0] + 1:], count_target[1:])
 
 
 def prob1():
